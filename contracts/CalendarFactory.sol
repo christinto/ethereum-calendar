f7f4e3f4d85bbc79fbcfe5fe262b991860892065
Nick <nickfarrell@gmail.com>
2018-05-21 18:39:28 -0700

    CalendarFactory refactored to use Mapped Structs with Delete-enabled Index + accompanying mocha tests

--- contracts/CalendarFactory.sol
+++ contracts/CalendarFactory.sol
@@ -3,10 +3,54 @@ pragma solidity ^0.4.18;
 import './Calendar.sol';
 
 contract CalendarFactory {
-  // An array of all the calendar contract instances
-  Calendar[] calendars;
+  // a struct containing a calendar contract plus pointer to keep track of it in array
+  struct CalendarStruct {
+    Calendar cal;
+    uint listPointer;
+  }
+  // this maps the addresses of the OWNERS of each calendar
+  mapping (address => CalendarStruct) public calendars;
+  // addresses of the owners of the calendars
+  address[] public calendarsList;
 
-  function createNewCalendar (address own) public {
-    calendars.push(new Calendar(address own));
+  // event for new calendar added
+  event NewCalendarAdded (address owner);
+  event CalendarDeleted (address owner);
+
+  // checks if a user with a given address already has a calendar
+  function hasCalendar (address calendarOwnerAddress) public constant returns (bool isIndeed) {
+    if(calendarsList.length == 0) return false;
+    return (calendarsList[calendars[calendarOwnerAddress].listPointer] == calendarOwnerAddress);
+  }
+
+  // Returns the count of calendars stored in calendarsList array
+  function getCalendarCount() public constant returns (uint){
+    return calendarsList.length;
+  }
+
+  function createNewCalendar (address calendarOwnerAddress) public returns (bool success) {
+    // user can only have 1 calendar per address
+    if (hasCalendar(calendarOwnerAddress)) revert();
+    calendars[calendarOwnerAddress].cal = new Calendar(calendarOwnerAddress);
+    calendars[calendarOwnerAddress].listPointer = calendarsList.push(calendarOwnerAddress) - 1;
+    emit NewCalendarAdded (calendarOwnerAddress);
+    return true;
+  }
+
+  function deleteCalendar (address calendarOwnerAddress) public returns (bool success) {
+    // make sure thers actually a calendar to delete
+    if (!hasCalendar(calendarOwnerAddress)) revert();
+    // ONLY THE OWNER OF A CALENDAR CAN DELETE IT!!!
+    if (calendarOwnerAddress != msg.sender) revert();
+
+    uint rowToDelete = calendars[calendarOwnerAddress].listPointer;
+    address keyToMove = calendarsList[calendarsList.length - 1];
+
+    calendarsList[rowToDelete] = keyToMove;
+    calendars[keyToMove].listPointer = rowToDelete;
+    calendarsList.length--;
+
+    emit CalendarDeleted (calendarOwnerAddress);
+    return true;
   }
 }
---
+++ test/calendar_factory.js
@@ -0,0 +1,78 @@
+var CalendarFactory = artifacts.require("./CalendarFactory.sol");
+
+contract('CalendarFactory', accounts => {
+
+  it("should assert true", done => {
+    let calendar_factory = CalendarFactory.deployed();
+    assert.isTrue(true);
+    done();
+  });
+
+  // contract deployed in migrations file with a value of 5 in contructor
+  it("getCalendarCount() returns 0 to begin with", () => {
+    let calendar_factory;
+    return CalendarFactory.deployed().then(instance => {
+      calendar_factory = instance;
+      return calendar_factory.getCalendarCount();
+    }).then((result) => {
+      assert.equal(result.toNumber(), 0);
+      //console.log(result.toNumber());
+      //console.log(result);
+    })
+  })
+
+  it("createNewCalendar() adds new Calendar contract instance to calendars array and emits an event", () => {
+    let calendar_factory;
+    return CalendarFactory.deployed().then(instance => {
+      calendar_factory = instance;
+      return calendar_factory.createNewCalendar(accounts[0]);
+    }).then((result) => {
+      assert.equal(result.logs[0].event, 'NewCalendarAdded');
+    }).then(() => {
+      return calendar_factory.getCalendarCount();
+    }).then((result) => {
+      //console.log((result));
+      assert.equal(result.toNumber(), 1);
+      //console.log(result.toNumber());
+      //console.log(result);
+    })
+  })
+
+  it("hasCalendar detects if someone trys to make a new calendar if they already got one", () => {
+    let calendar_factory;
+    return CalendarFactory.deployed().then(instance => {
+      calendar_factory = instance;
+      return calendar_factory.hasCalendar(accounts[0]);
+    }).then((result) => {
+      assert.isOk(result);
+    })
+  })
+
+  it("a user cant delete someone elses calendar", () => {
+    let calendar_factory;
+    return CalendarFactory.deployed().then(instance => {
+      calendar_factory = instance;
+      return calendar_factory.deleteCalendar(accounts[0], { from: accounts[1] });
+    })
+    // should just revert at this point
+  })
+
+  it("a user can delete their calendar", () => {
+    let calendar_factory;
+    return CalendarFactory.deployed().then(instance => {
+      calendar_factory = instance;
+      return calendar_factory.deleteCalendar(accounts[0], { from: accounts[0] });
+    }).then((result) => {
+      //console.log(result);
+      assert.equal(result.logs[0].event, 'CalendarDeleted');
+    }).then(() => {
+      return calendar_factory.getCalendarCount();
+    }).then((result) => {
+      //console.log((result));
+      assert.equal(result.toNumber(), 0);
+      //console.log(result.toNumber());
+      //console.log(result);
+    })
+  })
+
+});
